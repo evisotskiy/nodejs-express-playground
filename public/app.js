2c3d170bc467369440b5465747631b2740f0fc68
Evgene <evisotskiy@zaraffasoft.com>
2020-01-13 17:16:17 +0200

    Ready app without mongodb (used json files instead of db)

---
+++ public/app.js
@@ -0,0 +1,43 @@
+function formatPrice(price) {
+    return new Intl.NumberFormat('ru-RU', {
+        currency: 'rub',
+        style: 'currency'
+    }).format(price)
+}
+
+document.querySelectorAll('.price').forEach(node => {
+    node.textContent = formatPrice(node.textContent)
+});
+
+const $cart = document.querySelector('#cart');
+if ($cart) {
+    $cart.addEventListener('click', e => {
+        if (e.target.classList.contains('js-remove')) {
+            const id = event.target.dataset.id;
+
+            fetch('/cart/remove/' + id, {
+                method: 'delete'
+            })
+                .then(res => res.json())
+                .then(cart => {
+                    if (cart.courses.length) {
+                        const html = cart.courses.map(({ title, count, id }) => {
+                            return `
+                                <tr>
+                                    <td>${title}</td>
+                                    <td>${count}</td>
+                                    <td>
+                                        <button class="btn btn-small js-remove" data-id="${id}">Delete</button>
+                                    </td>
+                                </tr>
+                            `;
+                        }).join('')
+                        $cart.querySelector('tbody').innerHTML = html;
+                        $cart.querySelector('.price').textContent = formatPrice(cart.price);
+                    } else {
+                        $cart.innerHTML = '<p>Cart is empty</p>'
+                    }
+                });
+        }
+    })
+}
